0a724a4ce2ce37876a314a89d22a5b12be95ec9c
eliasalmeidaa <eliasmartinsdealmeda@gmail.com>
2026-07-07 15:29:26 -0300

    realizado declaração das variaveis

---
+++ frontend/js/script.js
@@ -0,0 +1,8 @@
+const campCliente = document.getElementById('campo-cliente');
+const campAparelho = document.getElementById('campo-apareho');
+const campDefeito = document.getElementById('campo-problema');
+const botaoOS = document.getElementById('botao-adicionar');
+const mensFeedback = document.getElementById('mensgaem-feedback');
+const contaOS = document.getElementById('contador-ordens');
+const listaOS = document.getElementById('lista-ordens');
+
